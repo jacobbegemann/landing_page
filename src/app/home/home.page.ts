03fd4cc31c6f9e6089e3898757cde413c589621e
Jacob Begemann <jacob_begemann@brown.edu>
2019-05-27 22:19:46 +0100

    new ionic page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertController: AlertController,
+    private toastController: ToastController) {
+      this.doIntermittentToast();
+     }
+
+  async doIntermittentToast() {
+    while (true) {
+      await this.sleep(60000);
+      this.presentToast();
+    }
+  }
+
+  async presentToast() {
+    console.log("toast");
+    const toast = await this.toastController.create({
+      message: 'Like this site? Leave a review.',
+      showCloseButton: true,
+      duration: 4000
+    });
+    toast.present();
+  }
+
+  async presentAlert() {
+    document.getElementById('message-box').value = "";
+    document.getElementById('message-box').placeholder = "Enter your message here...";
+    document.getElementById('personal').checked = false;
+    document.getElementById('professional').checked = false;
+    const alert = await this.alertController.create({
+      header: 'Thanks!',
+      message: 'Your message was sent.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 }
